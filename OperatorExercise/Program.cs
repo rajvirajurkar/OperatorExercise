0d47335e0a6bf84a1041ef1a1a9d39ac3c352dfc
rajvi rajurkar <rajvi.vm@gmail.com>
2022-06-01 07:39:52 -0700

    opertor ex

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 5;
+            int b = 10;
+            int c = a + b;
+
+            Console.WriteLine($" Value after adding a and b : {c}");
+            Console.WriteLine($" Value after sub a and b : {a - b}");
+
+        }
+    }
+}
+
